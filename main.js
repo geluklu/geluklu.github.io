693880ac0e89e065fcfd692e9e64d29bda68b1e5
geluklu <arcanarda55@gmail.com>
2023-04-02 13:10:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function button()
+{
+    const button=document.getElementById("button");
+    button.innerHTML="SELAMLAR";
+    //document.getElementById("button").innerHTML="ANANA";
+
+}
+function sifre(name){
+    const input=document.getElementById("i")
+
+}
